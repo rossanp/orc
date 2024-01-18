64e9adcd0fecb36cc386014b5404650f5967a280
Rossan P <rossan.p23@gmail.com>
2024-01-18 15:01:28 -0300

    Refatoração create table

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ con = lite.connect('dados.db')
 
 #Criando as tabelas
 # Tabela Categoria
-with con:
+""" with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE Categoria(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT)")
+    cur.execute("CREATE TABLE Categoria(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT)") """
 
 # Tabela Sub_Categoria
-with con:
+""" with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE SubCategoria(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, categoria_id INTEGER, nome TEXT, FOREIGN KEY(categoria_id) REFERENCES Categoria(id))")
+    cur.execute("CREATE TABLE SubCategoria(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT, categoria_id INTEGER, CONSTRAINT fk_categoria FOREIGN KEY(categoria_id) REFERENCES Categoria(id))") """
     
 # Tabela Banco
-with con:
+""" with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE Banco(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT, saldoinicial Numeric(10,2))")
+    cur.execute("CREATE TABLE Banco(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT, saldoinicial Numeric(10,2))") """
     
 # Tabela Lançamento
-with con:
+""" with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE Lancamento(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, categoria_id INTEGER, subcategoria_id INTEGER, banco_id INTEGER, data DATE, descricao TEXT, valor DECIMAL, tipo TEXT, FOREIGN KEY(categoria_id) REFERENCES Categoria(id), FOREIGN KEY(subcategoria_id) REFERENCES SubCategoria(id), FOREIGN KEY(banco_id) REFERENCES Banco(id))")
+    cur.execute("CREATE TABLE Lancamento(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, data DATE, descricao TEXT, valor DECIMAL, tipo TEXT, categoria_id INTEGER, subcategoria_id INTEGER, banco_id INTEGER, CONSTRAINT fk_categoria FOREIGN KEY(categoria_id) REFERENCES Categoria(id), CONSTRAINT fk_subcategoria FOREIGN KEY(subcategoria_id) REFERENCES SubCategoria(id), CONSTRAINT fk_BANCO FOREIGN KEY(banco_id) REFERENCES Banco(id))") """
